b7614ba0512fe5c9b54f2f5d24824eecf1ac4b77
Austin Delic <austin@austindelic.com>
2025-07-20 15:03:09 +0800

    init

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "Austin's Home Manager Linux Config";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { nixpkgs, home-manager, ... }: {
+    homeConfigurations.austin =
+      let
+        system = "x86_64-linux";
+        pkgs = import nixpkgs { inherit system; };
+      in
+      home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+        modules = [
+          {
+            home.username = "austin";
+            home.homeDirectory = "/home/austin";
+            home.stateVersion = "23.11";
+            targets.genericLinux.enable = true;
+            programs.home-manager.enable = true;
+            home.packages = with pkgs; [ git neovim gcc python3 rustup gh ];
+	    xdg.configFile."nvim".source = ./nvim;
+          }
+        ];
+      };
+  };
+}

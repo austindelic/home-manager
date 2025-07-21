6f35fd01c5a505759209452f164cdcf1a2960e30
Austin Delic <austin@austindelic.com>
2025-07-21 11:38:10 +0800

    test

--- modules/links.nix
+++ modules/links.nix
@@ -3,15 +3,13 @@
 { config, lib, ... }:
 
 let
-  # Absolute path to your editable config source dir
-  configDir = "${config.home.homeDirectory}/.config/home-manager/configs";
-
-  # Read all entries in the configs directory
+  # Relative path inside the flake
+  configDir = ./../configs;
   dotConfigs = builtins.attrNames (builtins.readDir configDir);
 in {
   home.file = builtins.listToAttrs (map (name: {
     name = ".config/${name}";
-    value.source = lib.file.mkOutOfStoreSymlink "${configDir}/${name}";
+    value.source = configDir + "/${name}";
   }) dotConfigs);
 }
 

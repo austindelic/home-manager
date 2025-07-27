047ba0a0e86e164237e15317146437757bacd426
Austin Delic <austin@austindelic.com>
2025-07-27 14:29:21 +0800

    updated test

--- modules/links.nix
+++ modules/links.nix
@@ -1,9 +1,15 @@
 # ~/.config/home-manager/modules/config_links.nix
 
-{ config, lib, ... }:
+{ config, ... }:
 
 {
-  home.file.".config/nvim".source = config.lib.file.mkOutOfStoreSymlink
-    "${config.home.homeDirectory}/.config/home-manager/configs/nvim";
+  home.file = {
+    ".config/nvim" = {
+      source = config.lib.file.mkOutOfStoreSymlink
+        "${config.home.homeDirectory}/.config/home-manager/configs/nvim";
+      recursive = true;
+    };
+
+  };
 }
 
